499cc750ff037704cf0187163023c280adce0641
Adrian Vladu <avladu@cloudbasesolutions.com>
2025-09-04 09:28:04 +0000

    openstack: add arch dependent images

--- openstack/configure-openstack.sh
+++ openstack/configure-openstack.sh
@@ -5,7 +5,14 @@ set -xe
 
 export KUBECONFIG=~/kub-poc.kubeconfig
 
-helm osh wait-for-pods openstack
+ARCH=amd64
+
+MODEL=$(uname -m)
+if [[ "${MODEL}" = "aarch64" ]]; then
+  ARCH=arm64
+fi
+
+# helm osh wait-for-pods openstack
 
 source ~/openstack-client/bin/activate
 
@@ -33,16 +40,16 @@ openstack --os-cloud openstack_helm security group rule create default --protoco
 openstack --os-cloud openstack_helm security group rule create default --protocol tcp --egress
 openstack --os-cloud openstack_helm security group rule create default --protocol icmp --egress
 
-openstack --os-cloud openstack_helm server create --image 'Cirros 0.6.2 64-bit' --flavor m1.tiny --network private --boot-from-volume 1 cirros-$rand_suffix
+openstack --os-cloud openstack_helm server create --image 'Cirros 0.6.2 64-bit' --flavor m1.tiny --network private cirros-$rand_suffix
 
-[ -f noble-server-cloudimg-amd64.img ] || \
-  wget https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img
+[ -f jammy-server-cloudimg-$ARCH.img ] || \
+  wget https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-$ARCH.img
 
-openstack --os-cloud openstack_helm image create ubuntu-noble --disk-format qcow2 --container-format bare --file noble-server-cloudimg-amd64.img
-openstack --os-cloud openstack_helm server create --image 'ubuntu-noble' --flavor m1.sylva --network private ubuntu-sylva --key sylva
+openstack --os-cloud openstack_helm image create ubuntu-jammy --disk-format qcow2 --container-format bare --file jammy-server-cloudimg-$ARCH.img
+openstack --os-cloud openstack_helm server create --image 'ubuntu-jammy' --flavor m1.sylva --network private ubuntu-sylva --key sylva
 
 [ -f flatcar_production_openstack_image.img ] || \
-  wget https://alpha.release.flatcar-linux.net/amd64-usr/current/flatcar_production_openstack_image.img
+  wget https://alpha.release.flatcar-linux.net/${ARCH}-usr/current/flatcar_production_openstack_image.img
 
 openstack --os-cloud openstack_helm image create flatcar-alpha --disk-format qcow2 --container-format bare --file flatcar_production_openstack_image.img
 openstack --os-cloud openstack_helm server create --image 'flatcar-alpha' --flavor m1.sylva --network private flatcar-alpha --key sylva
@@ -56,8 +63,8 @@ openstack --os-cloud openstack_helm server add floating ip flatcar-alpha 192.168
 
 until openstack --os-cloud openstack_helm console log show cirros-$rand_suffix | grep -i gocubsgo; do sleep 1 && echo 'Trying again'; done
 
-until nc -w5 -z -v 192.168.56.192 22; do sleep 1; done;
-until nc -w5 -z -v 192.168.56.195 22; do sleep 1; done;
+# until nc -w5 -z -v 192.168.56.192 22; do sleep 1; done;
+# until nc -w5 -z -v 192.168.56.195 22; do sleep 1; done;
 # chmod 600 sylva.pem
 # ssh -i sylva.pem ubuntu@192.168.56.192
 
